1652e1a564b38a8a207afdf4f8ac2c35c0e98b38
Kingjnr4 <onyiboixy@gmail.com>
2023-01-14 12:11:05 +0100

    init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function App() {
+   return (
+  <div className="w-screen h-screen flex justify-center items-center">
+    <p className="uppercase mx-auto">hello tailwind tauri next</p>
+  </div>
+  );
+}
+
+export default App;
